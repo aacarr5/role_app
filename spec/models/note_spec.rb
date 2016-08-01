c97262981bb4856679aba73352b8239a0027508f
Andrew Carr <aacarr5@gmail.com>
2016-08-01 11:18:11 -0500

    Fix error

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,1 +1,24 @@
 require 'rails_helper'
+
+RSpec.describe Note, type: :model do
+  let(:note) {FactoryGirl.create(:note)}
+
+  let(:category) {FactoryGirl.create(:category,name:"learning")}
+
+  let(:categorization) {FactoryGirl.create(:categorization,note_id:note.id,category_id:category.id)}
+
+
+  describe 'init' do 
+  	it 'should be a note' do 
+  		expect(note).to be_a Note
+  	end
+  end
+
+  describe 'associations' do 
+  	it 'should be associated to cateogry' do
+  		# binding.pry
+			expect(note.categories.first).to be_a Category
+	  end
+  end
+
+end
